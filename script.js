50601050a5b49750db18123ffac6fd6dce46c758
Sashini336 <46651489+Sashini336@users.noreply.github.com>
2023-06-20 11:00:09 +0300

    push

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const close = document.getElementById('close');
 const container = document.querySelector('.container');
 
 open.addEventListener('click', () => {
-  container.classList.add('open');
+  container.classList.add('show-nav');
 })
 
 
 close.addEventListener('click', () => {
-  container.classList.remove('open');
+  container.classList.remove('show-nav');
 })
